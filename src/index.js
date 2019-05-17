b2125a73f25a714cd211cebe7bab33b7bf874367
melonybox <grandmadcow@gmail.com>
2019-05-17 12:56:51 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,31 @@
 const addBtn = document.querySelector('#new-toy-btn')
 const toyForm = document.querySelector('.container')
+const allToys = document.querySelector('#toy-collection')
+const toyFormBtn = document.querySelector('.add-toy-form')
 let addToy = false
 
 // YOUR CODE HERE
+
+function onSubmit(){
+  const toyName = this.name.value
+  const toyImg = this.image.value
+  event.preventDefault();
+  fetch("http://localhost:3000/toys", {
+    method: "POST",
+    headers: {
+    "Content-Type": "application/json",
+    "Accept": "application/json"
+    },
+    body: JSON.stringify({ name: toyName, image: toyImg, likes: 0 })
+  })
+  .then(resR => resR.json())
+  .then(dataR => slapToyOnTheList(dataR))
+  toyForm.style.display = 'none'
+}
+
+function bindFormSubmit(){
+  toyFormBtn.addEventListener("submit", onSubmit)
+}
 
 addBtn.addEventListener('click', () => {
   // hide & seek with the form
@@ -10,10 +33,61 @@ addBtn.addEventListener('click', () => {
   if (addToy) {
     toyForm.style.display = 'block'
     // submit listener here
+    bindFormSubmit()
   } else {
     toyForm.style.display = 'none'
   }
 })
 
+function addLike() {
+  selfId = this.dataset.cardId
+  selfLikes = parseInt(this.dataset.cardLikes)
+  currentLikes = selfLikes + 1
+
+  fetch(`http://localhost:3000/toys/${ selfId }`, {
+    method: "PATCH",
+    headers: {
+    "Content-Type": "application/json",
+    "Accept": "application/json"
+    },
+    body: JSON.stringify({ likes: currentLikes })
+  })
+  .then(event.path[1].childNodes[2].innerText = `${ currentLikes } Likes`)
+  .then(event.path[1].childNodes[3].dataset.cardLikes = currentLikes)
+}
+
+function slapToyOnTheList(toy) {
+  const divTag = document.createElement("div")
+  const h2Tag = document.createElement("h2")
+  const imgTag = document.createElement("img")
+  const pTag = document.createElement("p")
+  const buttonTag = document.createElement("button")
+  h2Tag.innerText = toy.name
+  imgTag.src = toy.image
+  imgTag.className = "toy-avatar"
+  pTag.innerText = `${toy.likes} Likes`
+  buttonTag.innerText = "Like <3"
+  buttonTag.className = "like-btn"
+  buttonTag.dataset.cardId = toy.id
+  buttonTag.dataset.cardLikes = toy.likes
+  // buttonTag.dataset.displayLikes
+  buttonTag.addEventListener("click", addLike)
+  divTag.className = "card"
+  divTag.appendChild(h2Tag)
+  divTag.appendChild(imgTag)
+  divTag.appendChild(pTag)
+  divTag.appendChild(buttonTag)
+  allToys.appendChild(divTag)
+}
+
+function fillToyList(){
+  fetch("http://localhost:3000/toys")
+    .then(res => res.json())
+    .then(data => data.forEach(slapToyOnTheList))
+}
 
 // OR HERE!
+
+document.addEventListener("DOMContentLoaded", function(){
+ fillToyList()
+})
